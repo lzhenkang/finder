0fd6a6d37b44f4579616089d5236c011d43a53fa
leowzhenkang <leowzk@hotmail.com>
2020-03-05 16:04:46 +0800

    added devise and appointment controller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,21 @@
+class AppointmentsController < ApplicationController
+
+    def all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+end
